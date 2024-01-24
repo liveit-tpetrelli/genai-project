442996df62f186acf86bcbcef5b872e5dec1c105
liveit-tpetrelli <t.petrelli@reply.it>
2024-01-24 17:26:58 +0100

    better prompt and added sources to the answers

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from typing import Any
+from typing import Any, List
 
 import umap
 import numpy as np
@@ -8,6 +8,7 @@ from numpy._typing import _64Bit
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
+from MarkdownTextSplitter import MyMarkdownHeaderTextSplitter
 from configs.app_configs import AppConfigs
 
 from langchain_community.vectorstores import Chroma
@@ -15,7 +16,7 @@ from langchain_community.document_loaders import NotionDirectoryLoader
 
 from langchain.text_splitter import (MarkdownHeaderTextSplitter,
                                      SentenceTransformersTokenTextSplitter,
-                                     RecursiveCharacterTextSplitter)
+                                     RecursiveCharacterTextSplitter, MarkdownTextSplitter)
 
 from langchain_google_vertexai import (VertexAI,
                                        VertexAIEmbeddings)
@@ -34,7 +35,7 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.join(
 )
 
 
-def project_embeddings(_embeddings: list[list[float]], _umap_transform: umap.UMAP) \
+def project_embeddings(_embeddings: List[List[float]], _umap_transform: umap.UMAP) \
         -> np.ndarray[Any, np.dtype[np.floating[_64Bit]]]:
     umap_embeddings = np.empty((len(_embeddings), 2))
     for i, _emb in enumerate(tqdm(_embeddings)):
@@ -66,13 +67,21 @@ if __name__ == '__main__':
         loader = NotionDirectoryLoader(notion_persist_directory)
         docs = loader.load()
 
-        markdown_splitter = MarkdownHeaderTextSplitter(
+        # markdown_splitter = MarkdownHeaderTextSplitter(
+        #     headers_to_split_on=[("#", "Header 1"), ("##", "Header 2"), ("###", "Header 3")],
+        #     strip_headers=False
+        # )
+        md_splitter = MyMarkdownHeaderTextSplitter(
             headers_to_split_on=[("#", "Header 1"), ("##", "Header 2"), ("###", "Header 3")],
             strip_headers=False
         )
         md_header_splits = []
         for doc in docs:
-            md_header_splits += (markdown_splitter.split_text(doc.page_content))
+            # md_header_splits += (markdown_splitter.split_text(doc.page_content))
+            md_header_splits += md_splitter.split_text(
+                text=doc.page_content,
+                source=os.path.basename(doc.metadata['source'])
+            )
 
         text_splitter = RecursiveCharacterTextSplitter(  # separators=["\n\n", "\n", "(?<=\. )", " ", ""]
             chunk_size=600,
@@ -84,12 +93,13 @@ if __name__ == '__main__':
             tokens_per_chunk=200,
             chunk_overlap=30
         )
-        token_split_texts = token_splitter.split_documents(text_splits)
+        token_splits = token_splitter.split_documents(text_splits)
 
         vector_chromadb = Chroma.from_documents(
-            documents=token_split_texts,
+            documents=token_splits,
             persist_directory=chroma_persist_directory,
-            embedding=vertex_embeddings_model)
+            embedding=vertex_embeddings_model
+        )
     else:
         vector_chromadb = Chroma(
             persist_directory=chroma_persist_directory,
@@ -104,9 +114,9 @@ if __name__ == '__main__':
     #     print(document.page_content)
     #     print('\n')
     #
-    retrieved_embeddings1 = vector_chromadb.get(include=['embeddings'])['embeddings']
-    umap_transform = umap.UMAP(random_state=0, transform_seed=0).fit(retrieved_embeddings1)
-    projected_dataset_embeddings = project_embeddings(retrieved_embeddings1, umap_transform)
+    # retrieved_embeddings1 = vector_chromadb.get(include=['embeddings'])['embeddings']
+    # umap_transform = umap.UMAP(random_state=0, transform_seed=0).fit(retrieved_embeddings1)
+    # projected_dataset_embeddings = project_embeddings(retrieved_embeddings1, umap_transform)
     #
     # plt.figure()
     # plt.scatter(projected_dataset_embeddings[:, 0], projected_dataset_embeddings[:, 1], s=10)
@@ -138,35 +148,35 @@ if __name__ == '__main__':
     # plt.show()
 
     # TEST 3 - Expansion with generated answers
-    original_query = "Give me some materials about a topic i need to study."
-    hypothetical_answer = augment_query_generated(original_query)
-    joint_query = f"{original_query} {hypothetical_answer}"
-    # print(joint_query)
-
-    retrieved_documents3 = vector_chromadb.similarity_search(query=joint_query, k=5)
-
-    docs3 = []
-    for doc in retrieved_documents3:
-        docs3.append(doc.page_content)
-
-    retrieved_docs3_embeddings = vertex_embeddings_model.embed_documents(docs3)
-    original_query_embedding = vertex_embeddings_model.embed_query(original_query)
-    augmented_query_embedding = vertex_embeddings_model.embed_query(joint_query)
-
-    projected_original_query_embedding = project_embeddings([original_query_embedding], umap_transform)
-    projected_augmented_query_embedding = project_embeddings([augmented_query_embedding], umap_transform)
-    projected_retrieved_embeddings = project_embeddings(retrieved_docs3_embeddings, umap_transform)
-
-    plt.figure()
-    plt.scatter(projected_dataset_embeddings[:, 0], projected_dataset_embeddings[:, 1], s=10, color='gray')
-    plt.scatter(projected_retrieved_embeddings[:, 0], projected_retrieved_embeddings[:, 1], s=100, facecolors='none', edgecolors='g')
-    plt.scatter(projected_original_query_embedding[:, 0], projected_original_query_embedding[:, 1], s=150, marker='X', color='r')
-    plt.scatter(projected_augmented_query_embedding[:, 0], projected_augmented_query_embedding[:, 1], s=150, marker='X', color='orange')
-
-    plt.gca().set_aspect('equal', 'datalim')
-    plt.title(f'{original_query}')
-    plt.axis('off')
-    plt.show()
+    # original_query = "Give me some materials about a topic i need to study."
+    # hypothetical_answer = augment_query_generated(original_query)
+    # joint_query = f"{original_query} {hypothetical_answer}"
+    # # print(joint_query)
+    #
+    # retrieved_documents3 = vector_chromadb.similarity_search(query=joint_query, k=5)
+    #
+    # docs3 = []
+    # for doc in retrieved_documents3:
+    #     docs3.append(doc.page_content)
+    #
+    # retrieved_docs3_embeddings = vertex_embeddings_model.embed_documents(docs3)
+    # original_query_embedding = vertex_embeddings_model.embed_query(original_query)
+    # augmented_query_embedding = vertex_embeddings_model.embed_query(joint_query)
+    #
+    # projected_original_query_embedding = project_embeddings([original_query_embedding], umap_transform)
+    # projected_augmented_query_embedding = project_embeddings([augmented_query_embedding], umap_transform)
+    # projected_retrieved_embeddings = project_embeddings(retrieved_docs3_embeddings, umap_transform)
+    #
+    # plt.figure()
+    # plt.scatter(projected_dataset_embeddings[:, 0], projected_dataset_embeddings[:, 1], s=10, color='gray')
+    # plt.scatter(projected_retrieved_embeddings[:, 0], projected_retrieved_embeddings[:, 1], s=100, facecolors='none', edgecolors='g')
+    # plt.scatter(projected_original_query_embedding[:, 0], projected_original_query_embedding[:, 1], s=150, marker='X', color='r')
+    # plt.scatter(projected_augmented_query_embedding[:, 0], projected_augmented_query_embedding[:, 1], s=150, marker='X', color='orange')
+    #
+    # plt.gca().set_aspect('equal', 'datalim')
+    # plt.title(f'{original_query}')
+    # plt.axis('off')
+    # plt.show()
 
 
 
@@ -180,40 +190,51 @@ if __name__ == '__main__':
         return_messages=True,
         output_key='answer'
     )
-    llm = VertexAI(model_name="gemini-pro", temperature=0.1)
-    # memory = ConversationSummaryMemory(
-    #     llm=llm, memory_key="chat_history", return_messages=True
-    # )
+    llm = VertexAI(model_name="gemini-pro", temperature=0)
 
     # This controls how the standalone question is generated.
     # Should take `chat_history` and `question` as input variables.
-    condense_question_template = """Given the following chat history and a follow up question,\
-    rephrase the follow up input question to be a standalone question.
-    
-    Chat History:####
-    {chat_history}
-    ####
-
-    Follow Up Input: ####
-    {question}
-    ####
-    
-    Standalone question:"""
+    condense_question_template = (
+        "Given the following chat history and a follow up question, "
+        "rephrase the follow up input question to be a standalone question."
+        "\n\nChat History:####"
+        "\n{chat_history}"
+        "\n####"
+        "\n\nFollow Up Input: ####"
+        "\n{question}"
+        "\n####"
+        "\n\nStandalone question:"
+    )
 
     condense_question_prompt = PromptTemplate.from_template(template=condense_question_template)
 
+    # """
+    # If you find some external links or resources in the context, just DO NOT USE THEM and ask to the user if you need
+    # to access these resources."""
     combine_docs_template = """
-    Use the following pieces of context to answer the question at the end. 
-    If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    Use the following pieces of context to answer the question at the end.
+    Avoid to use any link or external resource if not explicitly said by the user.
+    You must retrieve as many information as possible from the context. You must use it to provide an helpful answer.
+    Pay attention to the format of the context. It might be in markdown or json. Try to parse the context very carefully to get as many data as possible.
+    You must act like a senior software engineer that must provide useful guidelines to newly recruited engineers.
     
-    {context}
+    
+    #### {context} ####
     
     
-    
-    If the chat history exists, and hence it is not empty, then USE IT to support your answer along with the context.
-    Otherwise, just use the context and add to your final answer that there is not any chat history.
+
+    If the chat history exists, and hence it is not empty, then try to use it to support your answer along with the context.
+    VERY IMPORTANT: just use the context and add information from the chat_history only if necessary to provide an helpful answer.
     
     {chat_history}
+
+    
+    Before you answer the question, try to follow this example:
+    ****
+    Q: "Can you give me the titles of just 3 easy code challenges about arrays?"
+    A: "1) move zeroes 2) two out of three 3) cells with odd values in a matrix
+    ****
+    
     
     Question: {question}
     Helpful Answer:"""
@@ -225,42 +246,48 @@ if __name__ == '__main__':
 
     qa = ConversationalRetrievalChain.from_llm(
         llm,
-        retriever=vector_chromadb.as_retriever(search_type="similarity", search_kwargs={"k": 1}),  # mmr
+        retriever=vector_chromadb.as_retriever(search_type="similarity", search_kwargs={"k": 5}),  # mmr
         memory=memory,
         condense_question_prompt=condense_question_prompt,
-        # return_source_documents=True,
+        return_source_documents=True,
         # return_generated_question=True,
         chain_type="stuff",
         combine_docs_chain_kwargs={"prompt": combine_docs_prompt},
         verbose=True
     )
 
-    # chat_history = []
-
-    question = "Can you give me 3 easy code challenges about arrays?"
+    question = "List 10 title of easy code challenges involving matrices."
+    # gen_answer = augment_query_generated(question)
+    # joint_question = (f"{question}"
+    #                   f"\n\nUse the following hypothetical answer as "
+    #                   f"an example for generating your actual answer: {gen_answer}")
     result = qa({"question": question})
-    # chat_history.extend([(question, result["answer"])])
-    # gen_question = result["generated_question"]
-    # src_docs = result["source_documents"]
+    sources = result["source_documents"]
     answer = result['answer']
 
-    # print(qa)
-    # print(result)
-    print(result['answer'])
+    all_metadata = [source.metadata for source in sources]
+    all_sources = [m['source'] for m in all_metadata]
+    if all_sources:
+        found_sources = set()
+        for source in all_sources:
+            source_name = source.strip()
+            found_sources.add(source_name)
 
-    question = "List only the titles."
-    result = qa({"question": question})
-    print(result['answer'])
-    # question = "Can you repeat the first option that you have just given to me?"
-    question = "What was the first option that you selected?"
-    result = qa({"question": question})
-    # chat_history.extend([(question, result["answer"])])
-    print(result['answer'])
+        if found_sources:
+            answer += f"\nSources: {', '.join(found_sources)}"
+        else:
+            answer += "\nNo sources found"
 
-    question = "No, not that sorry. What was the second one?"
-    result = qa({"question": question})
-    # chat_history.extend([(question, result["answer"])])
-    print(result['answer'])
+        print(answer)
 
-    # result = qa_chain({"query": question})
-    # print(result["result"])
+
+    question1 = "Can you give me the titles of just 3 easy code challenges about arrays?"
+    result1 = qa({"question": question1})
+    print(result1['answer'])
+    # question2 = "What was the first option that you selected?"
+    # result2 = qa({"question": question2})
+    # print(result2['answer'])
+    #
+    # question3 = "No, not that sorry. What was the second one?"
+    # result3 = qa({"question": question3})
+    # print(result3['answer'])
